5a484e7b63342301fd8a5a8a4cb2de8966dc7b78
KalilDev <kalilgamer0@gmail.com>
2021-10-20 01:09:24 -0300

    app_controller: Introduce maxLoadedWallpapers

--- lib/controllers/app_controller.dart
+++ lib/controllers/app_controller.dart
@@ -9,11 +9,14 @@ class AppController with ChangeNotifier {
 
   Color primaryColor = Colors.yellow;
   bool useDarkTheme = true;
+  int maxLoadedWallpapers = 90;
 
   // ignore: non_constant_identifier_names
   final APP_THEME_COLOR_PREF = "app_theme_color";
   // ignore: non_constant_identifier_names
   final APP_DARK_THEME_PREF = "app_dark_theme";
+  // ignore: non_constant_identifier_names
+  final APP_MAX_LOADED_WALLPAPERS_PREF = "app_max_loaded_wallpapers";
 
   AppController() {
     sharedPref.read<String>(APP_THEME_COLOR_PREF).then((value) {
@@ -22,6 +25,18 @@ class AppController with ChangeNotifier {
     sharedPref.read<bool>(APP_DARK_THEME_PREF).then((value) {
       if (value != null) changeTheme(value, persist: false);
     });
+    sharedPref.read<int>(APP_MAX_LOADED_WALLPAPERS_PREF).then((value) {
+      if (value != null) changeMaxLoadedWallpapers(value, persist: false);
+    });
+  }
+
+  void changeMaxLoadedWallpapers(int newValue, {bool persist = true}) {
+    maxLoadedWallpapers = newValue;
+    notifyListeners();
+
+    if (persist) {
+      sharedPref.save(APP_MAX_LOADED_WALLPAPERS_PREF, newValue);
+    }
   }
 
   void changePrimaryColor(String newPrimary, {bool persist = true}) {
